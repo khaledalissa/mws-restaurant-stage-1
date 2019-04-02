43a66cdf8484e381a873bd37dd5113d97f6e3659
Khaled Alissa <khaled@kimac.local>
2019-04-02 21:38:40 +0300

    second commit

--- js/main.js
+++ js/main.js
@@ -160,16 +160,17 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
-  image.alt = 'restaurant image'
+  image.alt = 'image for' + restaurant.name;
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h4');
   name.innerHTML = restaurant.name;
   li.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
+  neighborhood.role = 'neighborhood';
   li.append(neighborhood);
 
   const address = document.createElement('p');
@@ -209,4 +210,17 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     self.markers.push(marker);
   });
 } */
+
+
+
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js');
+}
+
+
+
+
+
+
 
---
+++ sw.js
@@ -0,0 +1,49 @@
+let cacheName = 'static-v1';
+
+self.addEventListener('install', function(event){
+	// open a cache and save all files
+	event.waitUntil(
+		caches.open(cacheName).then(function(cache){
+			return cache.addAll([
+					'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
+					'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
+					'/',
+					'/restaurant.html',
+					'css/styles.css',
+					'js/dbhelper.js',
+					'js/main.js',
+					'js/restaurant_info.js',
+					'data/restaurants.json',
+				]
+			);
+		})
+	);
+});
+
+
+self.addEventListener('activate', function(event){
+	event.waitUntil(
+		caches.keys().then(function(keys){
+			Promise.all(
+				keys.map(function(key){
+					if (key != cacheName){
+						return caches.delete(key);
+					}	
+				})
+			);
+		})
+	);
+});
+
+
+
+self.addEventListener('fetch', function(event){
+	event.respondWith(
+			caches.match(event.request).then(function(response){
+				return response || fetch(event.request);
+			})
+		);
+});
+
+
+
